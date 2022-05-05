a44d9ec91b384661b3ce495587efb4908a7d4e81
Dasha-p <62257569+Dasha-p@users.noreply.github.com>
2022-05-05 20:41:55 +0300

    Update db.py

--- homework06/db.py
+++ homework06/db.py
@@ -1,31 +1,31 @@
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+# mypy: ignore-errors
+from sqlalchemy import Column, Integer, String, create_engine  # mypy: ignore
+from sqlalchemy.ext.declarative import declarative_base  # mypy: ignore
+from sqlalchemy.orm import sessionmaker  # mypy: ignore
+
 from scraputils import get_news
+
 
 def create_database(lst):
     s = session()
-    slovar_nov = {'title': 'None',
-            'url': 'None',
-            'author': 'None',
-            'points': 0,
-            'comments': 0}
+    slovar_nov = {"title": "None", "url": "None", "author": "None", "points": 0, "comments": 0}
     for slovar in lst:
         if slovar != slovar_nov:
             new = News(
-                title=slovar['title'],
-                author=slovar['author'],
-                url=slovar['url'],
-                comments=slovar['comments'],
-                points=slovar['points']
+                title=slovar["title"],
+                author=slovar["author"],
+                url=slovar["url"],
+                comments=slovar["comments"],
+                points=slovar["points"],
             )
             s.add(new)
             s.commit()
 
+
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
 session = sessionmaker(bind=engine)
+
 
 class News(Base):
     __tablename__ = "news"
@@ -37,7 +37,8 @@ class News(Base):
     points = Column(Integer)
     label = Column(String)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
     url = "https://news.ycombinator.com/"
     news_list = get_news(url, n_pages=34)
